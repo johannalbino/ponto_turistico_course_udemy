0c039c9d2190f11b417936b6d7887cac38e39f8d
Johann Albino <johann.albino.ti@gmail.com>
2019-10-14 20:57:10 -0300

    Subindo para o Heroku

--- pontos_turisticos/settings.py
+++ pontos_turisticos/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['pontos-turist.herokuapp.com', 'localhost:8000']
+ALLOWED_HOSTS = ['pontos-tur.herokuapp.com', 'localhost:8000']
 
 
 # Application definition
