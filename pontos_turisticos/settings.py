19441ef3a63d23a1cb1db81ba7c636010f480241
Johann Albino <johann.albino.ti@gmail.com>
2019-10-14 22:00:27 -0300

    Reset Migrations

--- pontos_turisticos/settings.py
+++ pontos_turisticos/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'reset_migrations',
     'core',
     'atracoes',
     'comentarios',
